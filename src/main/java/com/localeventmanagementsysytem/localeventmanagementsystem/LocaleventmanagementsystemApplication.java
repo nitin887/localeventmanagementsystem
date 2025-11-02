69831c5f5c9b080dcb4b70e94370cf9fbc7543c3
nitin887 <nitinrao9213@gmail.com>
2025-11-02 10:23:18 +0530

    Resolved merge conflicts

---
+++ src/main/java/com/localeventmanagementsysytem/localeventmanagementsystem/LocaleventmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.localeventmanagementsysytem.localeventmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaleventmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaleventmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/localeventmanagementsysytem/localeventmanagementsystem/LocaleventmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localeventmanagementsysytem.localeventmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaleventmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
