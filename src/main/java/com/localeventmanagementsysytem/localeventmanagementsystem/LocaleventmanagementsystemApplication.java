429669595ed397241a7eee075ac1ef0b60321cea
nitin887 <nitinrao9213@gmail.com>
2025-11-09 09:08:59 +0530

    done

--- src/main/java/com/localeventmanagementsysytem/localeventmanagementsystem/LocaleventmanagementsystemApplication.java
+++ src/main/java/com/localeventmanagementsysytem/localeventmanagementsystem/LocaleventmanagementsystemApplication.java
@@ -1,9 +1,12 @@
 package com.localeventmanagementsysytem.localeventmanagementsystem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Local Event Management System API", version = "1.0", description = "API documentation for the Local Event Management System"))
 public class LocaleventmanagementsystemApplication {
 
 	public static void main(String[] args) {
